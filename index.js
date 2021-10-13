04ace3a5645b2f09b4de756d811799471a0e9798
Mohammed Hussian <mohussain237@gmail.com>
2021-10-13 12:39:55 +0100

    DB connection and body parser

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const authRoute = require('./routes/auth')
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 
+//connection to DB
+mongoose.connect(process.env.DB_CONN, { useNewUrlParser: true }, () =>
+console.log("DATA_BASE Connection is Successful....")
+);
 
+// middleware for reading data from postman
+app.use(express.json());
 
-app.use('/api/user', authRoute)
+// Routers import
+const authRoute = require("./routes/Auth");
 
-const PORT = process.env.PORT || 5000
+// Route MiddleWare....
+app.use("/api/user", authRoute);
 
-app.listen(PORT, () =>console.log(`server is listning on on ${PORT}` ) )
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`SERVER is Listning On ${PORT}....!!`));
