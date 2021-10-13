30cdd6f4e5d31ba38440fdc0099b674d4e0641da
Mohammed Hussian <mohussain237@gmail.com>
2021-10-13 13:04:16 +0100

    adding register data and save in DB

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,28 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const User = require("../model/User");
+const {registerValidation, loginValidation} = require('../validation')
 
 
-router.post('/register', (req, res) => {
-    res.send ("we are at Register")
-})
+router.post("/register", (req, res) => {
 
-router.post('/login', (req, res) => {
-    res.send ("login bla bla page")
-})
+    const {error} = registerValidation (req.body)
+    if (error) return res.status(400).send(error.details[0].massage)
+     
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.post("/login", (req, res) => {
+  res.send("login bla bla page");
+});
 
 module.exports = router;
